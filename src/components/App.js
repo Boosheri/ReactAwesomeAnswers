f6b95009daf06c46e36b8bcdc6fdaa399028b4f6
Nina Boosheri <nboosheri@gmail.com>
2019-05-10 15:35:16 -0700

    create navbar and css for navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { QuestionShowPage } from "./QuestionShowPage";
 import { QuestionIndexPage } from "./QuestionIndexPage";
-import { Clock } from "./Clock";
 import { WelcomePage } from "./WelcomePage";
+import { NavBar } from "./NavBar";
 
 // In React application, we create a component that acts as the
 // "root" or the entry point to all of our other components.
@@ -12,7 +12,9 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <Clock />
+        <header>
+          <NavBar />
+        </header>
         <Route exact path="/" component={WelcomePage} />
         <Route exact path="/questions" component={QuestionIndexPage} />
         <Route path="/questions/:id" component={QuestionShowPage} />
