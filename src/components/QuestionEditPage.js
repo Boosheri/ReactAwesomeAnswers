6c3665b66f9cdf2a09b7373eb1627b2bd6b699d5
Nina Boosheri <nboosheri@gmail.com>
2019-05-14 16:00:17 -0700

    refactor QuestionForm

---
+++ src/components/QuestionEditPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class QuestionEditPage extends Component {
+  render() {
+    return (
+      <main className="Page">
+        <h1>Question Edit Page</h1>
+      </main>
+    );
+  }
+}
