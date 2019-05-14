4e084b075fe84ce78d7acfa8ae458437f0f2fb4a
Nina Boosheri <nboosheri@gmail.com>
2019-05-14 14:56:18 -0700

    add loading state while waiting for compenents to fetch data

--- src/components/QuestionNewPage.js
+++ src/components/QuestionNewPage.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import { Question } from '../api/question';
-import { Session } from '../api/session';
+import React, { Component } from "react";
+import { Question } from "../api/question";
+import { Session } from "../api/session";
+import { FormErrors } from "./FormErrors";
 
 export class QuestionNewPage extends Component {
+  state = {
+    errors: []
+  };
+
   createQuestion(event) {
     event.preventDefault();
     const { currentTarget } = event;
     const formData = new FormData(currentTarget);
 
     Question.create({
-      title: formData.get('title'),
-      body: formData.get('body'),
-    })
-    .then((data) => {
+      title: formData.get("title"),
+      body: formData.get("body")
+    }).then(data => {
       if (!data.errors) {
         // The `history` prop is provide by the <Route>
         // component. It has a bunch of methods to manipulate
         // browser. You can use `push` to direct a user to any
         // page in our app.
         this.props.history.push(`/questions/${data.id}`);
+      } else {
+        console.log(data.errors);
+        this.setState({
+          errors: data.errors
+        });
       }
     });
   }
@@ -27,7 +36,10 @@ export class QuestionNewPage extends Component {
     return (
       <main className="Page">
         <h1>Ask a Question</h1>
-        <form onSubmit={(event) => this.createQuestion(event)}>
+
+        <FormErrors errors={this.state.errors} />
+
+        <form onSubmit={event => this.createQuestion(event)}>
           <div>
             <label htmlFor="title">Title</label> <br />
             <input name="title" id="title" />
