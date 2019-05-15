fd6d231c8521f821ae0f84f4b0c9a7db52f0c1c6
Nina Boosheri <nboosheri@gmail.com>
2019-05-15 15:37:26 -0700

    add office ui fabric

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 // To use CSS files in a create-react-app based project,
 // import the file as you would a JS file.
-// A tool called Webpack will take imported CSS and make it appear
-// as a <style /> tag in any page where this JS is used.
+// A tool called Webpack will take imported CSS and
+//  make it appear as a <style /> tag in any page
+// where this JS is used.
 import "./index.css";
 import { App } from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+
+// Tell uifabric to load icons
+import { initializeIcons } from "@uifabric/icons";
+initializeIcons();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
